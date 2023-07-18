5658da22dc5c2696f86c4e4edef2fab92736696b
demostanis <demostanis@protonmail.com>
2023-07-18 15:08:27 +0200

    Add hover effect on titlebars

--- airootfs/etc/skel/.config/awesome/titlebar.lua
+++ airootfs/etc/skel/.config/awesome/titlebar.lua
@@ -8,6 +8,34 @@ return function(c)
         end)
     )
 
+    -- Same logic as in dock.lua
+    local titlebar_button_with_hover_effect = function(button)
+        local should_lighten = false
+        local initial_draw = button.draw
+        function button.draw(self, _, cr, ...)
+            initial_draw(self, _, cr, ...)
+            local color = beautiful.fg_normal
+            if should_lighten then
+                color = beautiful.wibar_widget_hover_color
+            end
+            local s = self._private.image
+            cr:set_source_rgba(gears.color.parse_color(color))
+            cr:mask_surface(gears.surface(s), 0, 0)
+            cr:fill()
+        end
+        button:connect_signal("mouse::enter", function()
+            should_lighten = true
+            button:emit_signal(
+                "widget::redraw_needed")
+        end)
+        button:connect_signal("mouse::leave", function()
+            should_lighten = false
+            button:emit_signal(
+                "widget::redraw_needed")
+        end)
+        return button
+    end
+
     awful.titlebar(c):setup{
         {
             {
@@ -30,19 +58,19 @@ return function(c)
             {
                 -- a bunch of random margins because these images' size suck...
                 {
-                    awful.titlebar.widget.ontopbutton(c),
+                    titlebar_button_with_hover_effect(awful.titlebar.widget.ontopbutton(c)),
                     widget = wibox.container.margin,
-                    right = 8,
+                    right = 7,
                     top = 2
                 },
                 {
-                    awful.titlebar.widget.maximizedbutton(c),
+                    titlebar_button_with_hover_effect(awful.titlebar.widget.maximizedbutton(c)),
                     widget = wibox.container.margin,
                     top = 1,
                     bottom = 4,
                 },
                 {
-                    awful.titlebar.widget.closebutton(c),
+                    titlebar_button_with_hover_effect(awful.titlebar.widget.closebutton(c)),
                     widget = wibox.container.margin,
                     bottom = 2.5
                 },

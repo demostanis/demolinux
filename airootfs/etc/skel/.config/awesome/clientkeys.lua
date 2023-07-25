fdeba8e747775e8355e66cfd8ab9fec76c6e467e
demostanis <demostanis@protonmail.com>
2023-07-25 15:08:09 +0200

    Change keybinding to maximize window

--- airootfs/etc/skel/.config/awesome/clientkeys.lua
+++ airootfs/etc/skel/.config/awesome/clientkeys.lua
@@ -2,7 +2,7 @@ return gears.table.join(
     awful.key({ modkey }, "q", function(c) c:kill() end),
     awful.key({ modkey }, "o", function(c) c:move_to_screen() end),
     awful.key({ modkey }, "m", function(c) c.minimized = true end),
-    awful.key({ modkey }, "f", function(c)
+    awful.key({ modkey }, "w", function(c)
         c.maximized = not c.maximized
         c:raise()
     end)

36d695e1f8572e535e5620083fd3583887c4e1a3
demostanis <demostanis@protonmail.com>
2023-08-06 18:34:36 +0200

    Switch to Oomox for GTK theming

---
+++ airootfs/opt/oomox/run_oomox.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+DEST=${DEST:-/etc/skel/.themes}
+OOMOX_PATH=/opt/oomox
+
+# TODO: use CUSTOM_PATHLIST
+
+python -c "
+import sys, gi
+sys.path.append('$OOMOX_PATH/')
+gi.require_version('Gtk', '3.0')
+from plugins.base16.oomox_plugin import Plugin, convert_oomox_to_base16
+from oomox_gui.theme_file_parser import read_colorscheme_from_path
+plugin_path = '$OOMOX_PATH/plugins/base16/schemes/mellow/mellow-purple.yaml'
+result = []
+themix_theme = None
+read_colorscheme_from_path(plugin_path, callback=result.append)
+for item in result:
+	themix_theme = item
+base16_theme = convert_oomox_to_base16(colorscheme=themix_theme)
+for oomox_key, base16_key in Plugin.translation_dark.items():
+	if base16_key in base16_theme:
+	    themix_theme[oomox_key] = base16_theme[base16_key]
+base16_theme = convert_oomox_to_base16(colorscheme=themix_theme)
+for a, b in base16_theme.items():
+	if a.startswith('themix_') and a != 'themix_ICONS_STYLE':
+		a = a.replace('themix_', '')
+		print(f'{a}={b}', file=sys.stderr)
+print(open('$OOMOX_PATH/additions').read(), file=sys.stderr, end='')
+" 2>&1 >/dev/null | grep -v Authorization | \
+	"$OOMOX_PATH"/plugins/theme_oomox/change_color.sh \
+		-o oomox-mellow-purple -t "$DEST" /dev/stdin
+
+mkdir -p "$DEST"/oomox-mellow-purple/gtk-4.0
+themix-base16-cli \
+	"$OOMOX_PATH"/plugins/base16/templates/gtk4/templates/gtk.mustache \
+	"$OOMOX_PATH"/plugins/base16/schemes/mellow/mellow-purple.yaml \ |
+	grep -v ^ERROR: | grep -v '^Import Colors' \
+	> "$DEST"/oomox-mellow-purple/gtk-4.0/gtk-dark.css

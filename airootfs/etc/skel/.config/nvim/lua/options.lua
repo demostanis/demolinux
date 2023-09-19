615a3c4e7346dafd12ce591b9bf757767712dd29
demostanis worlds <demostanis@protonmail.com>
2023-09-19 03:16:05 +0200

    Misc Neovim options

--- airootfs/etc/skel/.config/nvim/lua/options.lua
+++ airootfs/etc/skel/.config/nvim/lua/options.lua
@@ -1,3 +1,13 @@
 vim.cmd[[colorscheme base16-mellow-purple]]
 vim.o.mousemodel = "extend"
 vim.g.mapleader = " "
+vim.o.relativenumber = true
+vim.o.updatetime = 100
+vim.o.signcolumn = "yes"
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    callback = function()
+        vim.o.relativenumber = false
+        vim.o.signcolumn = "no"
+    end
+})

81919e9b33a177916a66e053dd758e712add9e03
demostanis <demostanis@protonmail.com>
2023-09-24 03:16:55 +0200

    Autospawn the Flameshot daemon on startup
    That's needed to copy to clipboard... Why are there no errors...

--- airootfs/etc/skel/.config/awesome/startup/autospawn.lua
+++ airootfs/etc/skel/.config/awesome/startup/autospawn.lua
@@ -1,4 +1,8 @@
 autospawn("picom-wrapper")
 autospawn("parcellite")
+-- Flameshot needs its daemon to be
+-- spawned in order to be able to copy
+-- screenshots to the clipboard.
+autospawn("flameshot")
 
 -- vim:set et sw=4 ts=4:

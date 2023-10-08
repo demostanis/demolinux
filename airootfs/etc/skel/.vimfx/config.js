5730dd5022c213c1629d90049c73a295db93e70d
demostanis worlds <demostanis@protonmail.com>
2023-10-08 02:53:02 +0200

    Remap Ctrl-W to delete backward word in Firefox
    Woohooooooooooooo no more accidental tab closingggggsgs

--- airootfs/etc/skel/.vimfx/config.js
+++ airootfs/etc/skel/.vimfx/config.js
@@ -33,3 +33,32 @@ vimfx.addKeyOverrides(
 	[ () => true, // is that the right way of doing it..?
 		['<space>', 'r']]
 )
+
+// copied from https://superuser.com/a/1747680
+try {
+  let { classes: Cc, interfaces: Ci, manager: Cm  } = Components;
+  const Services = globalThis.Services;
+  const {SessionStore} = Components.utils.import('resource:///modules/sessionstore/SessionStore.jsm');
+  function ConfigJS() { Services.obs.addObserver(this, 'chrome-document-global-created', false); }
+  ConfigJS.prototype = {
+    observe: function (aSubject) { aSubject.addEventListener('DOMContentLoaded', this, {once: true}); },
+    handleEvent: function (aEvent) {
+      let document = aEvent.originalTarget; let window = document.defaultView; let location = window.location;
+      if (/^(chrome:(?!\/\/(global\/content\/commonDialog|browser\/content\/webext-panels)\.x?html)|about:(?!blank))/i.test(location.href)) {
+        if (window._gBrowser) {
+
+		// this is not really VimFx related
+		// but i didn't know where to put it...
+		// 
+		// this disables Ctrl-W to close a tab and replaces
+		// it with readline-like behavior, deleting the word
+		// behind the cursor.
+		window.hijackCtrlW = () => window.goDoCommand('cmd_deleteWordBackward')
+		document.querySelector('#cmd_close').setAttribute('oncommand', 'hijackCtrlW()')
+
+        }
+      }
+    }
+  };
+  if (!Services.appinfo.inSafeMode) { new ConfigJS(); }
+} catch(ex) {};

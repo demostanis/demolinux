38973f47379929aca2cc7fe03bc84c1997f9b537
demostanis <demostanis@protonmail.com>
2023-11-12 00:12:44 +0000

    Add a timeout before starting Picom

--- airootfs/etc/skel/.config/awesome/startup/autospawn.lua
+++ airootfs/etc/skel/.config/awesome/startup/autospawn.lua
@@ -1,8 +1,16 @@
-autospawn("picom-wrapper")
 autospawn("clipster -d")
 -- Flameshot needs its daemon to be
 -- spawned in order to be able to copy
 -- screenshots to the clipboard.
 autospawn("flameshot")
+
+-- We need a small timeout because
+-- Picom acts weird on first start.
+gears.timer{
+    timeout = 1000,
+    callback = function()
+        autospawn("picom-wrapper")
+    end
+}:start()
 
 -- vim:set et sw=4 ts=4:

e81ae3b35bc9d6fe0e61dddb6fd87fa223875488
demostanis <demostanis@protonmail.com>
2024-06-25 02:24:48 +0200

    Improve neovim configuration with gitsigns and telescope tabs

--- airootfs/etc/skel/.config/nvim/lua/plugins/telescope.lua
+++ airootfs/etc/skel/.config/nvim/lua/plugins/telescope.lua
@@ -13,3 +13,6 @@ local telescope = require"telescope.builtin"
 vim.keymap.set("n", "<leader>f", telescope.find_files)
 vim.keymap.set("n", "<leader>g", telescope.live_grep)
 vim.keymap.set("n", "<leader>b", telescope.buffers)
+
+local tabs = require"telescope-tabs"
+vim.keymap.set("n", "<leader>t", tabs.list_tabs)

958a8e841ac9788cbd7b7222d4b279d7890afa9d
demostanis <demostanis@protonmail.com>
2024-12-09 14:20:56 +0100

    Remove awesome-switcher

--- airootfs/etc/skel/.config/awesome/startup/init.lua
+++ airootfs/etc/skel/.config/awesome/startup/init.lua
@@ -2,6 +2,5 @@ require"startup/errors"
 require"startup/prefs"
 require"startup/autospawn"
 require"startup/tagpreview"
-require"startup/switcher"
 
 -- vim:set et sw=4 ts=4:
--- airootfs/etc/skel/.config/awesome/startup/switcher.lua
+++
@@ -1,4 +0,0 @@
-switcher.settings.preview_box_bg = beautiful.bg_focus
-switcher.settings.preview_box_border = beautiful.bg_focus
-switcher.settings.preview_box_title_color = {1, 1, 1, 1}
-

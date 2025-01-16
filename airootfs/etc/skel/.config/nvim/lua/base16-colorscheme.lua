939c8329f5a04502e1ab2bce1a1b2ba9a61145d7
demostanis <demostanis@protonmail.com>
2025-01-16 01:19:52 +0100

    nvim: alias TSComment to Comment to make it more readable

--- airootfs/etc/skel/.config/nvim/lua/base16-colorscheme.lua
+++ airootfs/etc/skel/.config/nvim/lua/base16-colorscheme.lua
@@ -284,7 +284,7 @@ function M.setup(colors, config)
     hi.TSAttribute                        = { guifg = M.colors.base0A, guibg = nil, gui = 'none', guisp = nil }
     hi.TSBoolean                          = { guifg = M.colors.base09, guibg = nil, gui = 'none', guisp = nil }
     hi.TSCharacter                        = { guifg = M.colors.base08, guibg = nil, gui = 'none', guisp = nil }
-    hi.TSComment                          = { guifg = M.colors.base03, guibg = nil, gui = 'italic', guisp = nil }
+    hi.TSComment                          = 'Comment'
     hi.TSConstructor                      = { guifg = M.colors.base0D, guibg = nil, gui = 'none', guisp = nil }
     hi.TSConditional                      = { guifg = M.colors.base0E, guibg = nil, gui = 'none', guisp = nil }
     hi.TSConstant                         = { guifg = M.colors.base09, guibg = nil, gui = 'none', guisp = nil }

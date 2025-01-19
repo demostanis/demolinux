a899e088f9591c36f35212ac66d0ebbf5292b6d4
demostanis <demostanis@protonmail.com>
2025-01-19 23:06:07 +0100

    awesome: add fullscreen keybind

--- airootfs/etc/skel/.config/awesome/clientkeys.lua
+++ airootfs/etc/skel/.config/awesome/clientkeys.lua
@@ -2,7 +2,8 @@ return gears.table.join(
     awful.key({ modkey }, "q", function(c) c:kill() end),
     awful.key({ modkey }, "o", function(c) c:move_to_screen() end),
     awful.key({ modkey }, "m", function(c) c.minimized = true end),
-    awful.key({ modkey }, "w", function(c) layout.maximize(c) end)
+    awful.key({ modkey }, "w", function(c) layout.maximize(c) end),
+    awful.key({ modkey, "Shift" }, "f", function(c) c.fullscreen = not c.fullscreen end)
 )
 
 -- vim:set et sw=4 ts=4:

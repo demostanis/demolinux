32a87f4ebc31196ae7a317b89ac622c5504af7d3
demostanis <demostanis@protonmail.com>
2025-02-22 23:31:23 +0100

    nvim: add navic plugin

--- airootfs/etc/skel/.config/nvim/lua/plugins/lualine.lua
+++ airootfs/etc/skel/.config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,13 @@
 require"lualine".setup{
     -- remove useless information
-    sections = {lualine_b = {}, lualine_x = {}}
+    sections = {lualine_b = {}, lualine_x = {}},
+	winbar = {
+        lualine_c = {
+            {
+                "navic",
+                color_correction = nil,
+                navic_opts = nil
+            }
+        }
+    }
 }

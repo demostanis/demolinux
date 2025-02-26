6efd01e4c83c27cfc8fe3fb9214088b839024883
demostanis <demostanis@protonmail.com>
2025-02-26 22:36:13 +0100

    nvim: bind U to Telescope resume

--- airootfs/etc/skel/.config/nvim/lua/plugins/telescope.lua
+++ airootfs/etc/skel/.config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,7 @@ local telescope = require"telescope.builtin"
 vim.keymap.set("n", "<leader>f", telescope.find_files)
 vim.keymap.set("n", "<leader>g", telescope.live_grep)
 vim.keymap.set("n", "<leader>b", telescope.buffers)
+vim.keymap.set("n", "U", telescope.resume)
 
 local tabs = require"telescope-tabs"
 vim.keymap.set("n", "<leader>t", tabs.list_tabs)

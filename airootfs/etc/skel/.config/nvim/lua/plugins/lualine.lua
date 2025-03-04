c1d024dbcbff7cbfe9331cf1f543b08ee275a4f9
demostanis <demostanis@protonmail.com>
2025-03-04 14:41:35 +0100

    nvim: prevent the buffer from swinging

--- airootfs/etc/skel/.config/nvim/lua/plugins/lualine.lua
+++ airootfs/etc/skel/.config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,14 @@
 require"lualine".setup{
     -- remove useless information
     sections = {lualine_b = {}, lualine_x = {}},
-	winbar = {
+    winbar = {
         lualine_c = {
             {
                 "navic",
                 color_correction = nil,
                 navic_opts = nil
-            }
+            },
+            { "filename" }
         }
     }
 }

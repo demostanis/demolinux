4ad3bbfa29ed48e0abb7856999d9bf152429e61b
demostanis <demostanis@protonmail.com>
2026-02-08 23:42:47 +0100

    nvim: fix a buffer-term bug

--- airootfs/etc/skel/.config/nvim/lua/plugins/term.lua
+++ airootfs/etc/skel/.config/nvim/lua/plugins/term.lua
@@ -2,4 +2,12 @@ local term = require"buffer-term"
 
 term.setup{}
 
-vim.keymap.set({ "n", "t" }, "<c-t>", function() term.toggle('a') end)
+vim.keymap.set({ "n", "t" }, "<c-t>", function()
+	if vim.api.nvim_buf_get_name(0) == "" and
+		not vim.api.nvim_buf_get_option(0, "modified") then
+		-- buffer-term has a bug when the current buffer's name is empty,
+		-- we have to toggle it twice...
+		term.toggle('a')
+	end
+	term.toggle('a')
+end)

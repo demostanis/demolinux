913467497263c4f7c8943db6750a3fbc47ce96fd
demostanis <demostanis@protonmail.com>
2026-09-06 00:57:59 +0200

    ci: report individual guest-test timings on successful runs

--- .github/run-tests.sh
+++ .github/run-tests.sh
@@ -55,7 +55,7 @@ for index in "${!pids[@]}"; do
         cat ".ci/tests/${names[$index]}/tests.log"
         failed=1
     fi
-    grep '^VM harness took' ".ci/tests/${names[$index]}/tests.log" || true
+    grep -E '^VM SSH ready|^PASS test |^VM harness took' ".ci/tests/${names[$index]}/tests.log" || true
 done
 pids=()
 exit "$failed"

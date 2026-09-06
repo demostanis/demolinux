f6aeff7836d2163df9f3f0f544ea2171bb5f952c
demostanis <demostanis@protonmail.com>
2026-09-06 02:54:56 +0200

    tests: handle Firefox debugger framing and asynchronous replies
    Read complete byte-counted packets and correlate evaluation acknowledgements with results. Bound debugger calls and launch on about:blank to avoid startup navigation races.

--- tests/firefox/browsereval.py
+++ tests/firefox/browsereval.py
@@ -1,89 +1,25 @@
 import socket
 import time
 from subprocess import check_output
-from json import dumps, loads
+from protocol import encode_packet, evaluate, receive_packet, receive_reply
 
 fhost = "localhost"
 fport = 6000
 
-def fpacket(data):
-    stringified = dumps(data)
-    return bytes(f"{len(stringified)}:{stringified}", "utf-8")
-
-def rfpacket(s):
-    plen = 0
-    while True:
-        n = s.recv(1)
-        if n == b':':
-            break
-        plen *= 10
-        plen += int(n)
-
-    data = s.recv(plen)
-    if b"frameUpdate" in data:
-        return rfpacket(s)
-    return loads(data)
-
-def process_packet():
-    return fpacket(
-        {"type":"getProcess","id":0,"to":"root"}
-    )
-
-def target_packet(desc):
-    return fpacket(
-        {"type":"getTarget","to":desc}
-    )
-
-def eval_packet(code,console):
-    code = "(async()=>{return JSON.stringify("+code+")})();"
-    return fpacket(
-        {"type":"evaluateJSAsync","text":code,"to":console}
-    )
-
-def recv_process(s):
-    res = rfpacket(s)
-    if not "processDescriptor" in res:
-        raise Exception("bad json data: no processDescriptor")
-    return res["processDescriptor"]["actor"]
-
-def recv_target(s):
-    res = rfpacket(s)
-    if not "process" in res or not "consoleActor" in res["process"]:
-        raise Exception("bad json data: no consoleActor")
-    return res["process"]["consoleActor"]
-
-def recv_eval(s):
-    res = rfpacket(s)
-    if "exceptionMessage" in res:
-        raise Exception(res["exceptionMessage"])
-    if not "result" in res:
-        raise Exception("bad json data: no result")
-    try:
-        # what the fuck
-        return loads(res["result"]["preview"]["ownProperties"]["<value>"]["value"])
-    except:
-        return res["result"]
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.settimeout(15)
     s.connect((fhost, fport))
 
     # ignore first packet
-    rfpacket(s)
+    receive_packet(s)
 
-    s.sendall(process_packet())
-    desc = recv_process(s)
-    s.sendall(target_packet(desc))
-    console = recv_target(s)
+    s.sendall(encode_packet({"type": "getProcess", "id": 0, "to": "root"}))
+    desc = receive_reply(s, "processDescriptor")["processDescriptor"]["actor"]
+    s.sendall(encode_packet({"type": "getTarget", "to": desc}))
+    console = receive_reply(s, "process")["process"]["consoleActor"]
 
     def eval(code):
-        s.sendall(eval_packet(code, console))
-        # wait for resultID
-        while True:
-            res = rfpacket(s)
-            if "resultID" in res:
-                break
-
-        return recv_eval(s)
+        return evaluate(s, console, code)
 
     enabled_addons = eval("""
             await (async () => {
@@ -103,7 +39,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     assert "VimFx-unlisted@akhodakivskiy.github.com" in enabled_addons
     assert "CanvasBlocker@kkapsner.de" in enabled_addons
     assert "mellow-purple@demostanis" in enabled_addons
-    assert "{446900e4-71c2-419f-a6a7-df9c091e268b}" in enabled_addons # Bitwarden
+    assert "{446900e4-71c2-419f-a6a7-df9c091e268b}" in enabled_addons  # Bitwarden
 
     # taken from https://searchfox.org/mozilla-central/source/toolkit/content/aboutSupport.js#1670
     eval("""
